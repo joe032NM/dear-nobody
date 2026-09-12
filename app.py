1af99e82f45f8e2126013ae18e3a83db7ac81ff4
joe032NM <suryapriyamalu@gmail.com>
2026-09-12 16:12:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -33,7 +33,9 @@ class Story(db.Model):
     content = db.Column(db.Text, nullable=False)
     author = db.Column(db.String(100), nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
-
+    
+with app.app_context():
+    db.create_all()
 
 # -----------------------------
 # Home
