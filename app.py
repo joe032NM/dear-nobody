9237bb192a2982cbe20f54f36e98ccebf11ffef3
joe032NM <suryapriyamalu@gmail.com>
2026-09-12 16:10:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,8 +14,7 @@ app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
-with app.app_context():
-    db.create_all()
+
 
 
 # -----------------------------
